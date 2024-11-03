3524c1547e07385e1c222a68194da494f03102de
Lipatov.Ilya <lipatoff.ilya99@mail.ru>
2024-11-04 00:50:21 +0300

    complete part 5 (add new game: Even)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,9 +2,26 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        var name = Cli.getName();
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n2 - Even\n0 - Exit");
+        var startingChoice = Cli.getNum();
+        System.out.println("Your choice: " + startingChoice);
+
+        if (startingChoice == 1) { // 1 - Greet
+            Cli.getGreet();
+        }
+
+        else if (startingChoice == 2) { // 2 - Even ("Игра: Проверка на чётность")
+            Cli.getGreet();
+            Even.getEven();
+        }
+        else if (startingChoice == 0) {
+            System.out.println("See ya! :)");
+        }
+        else {
+            System.out.println("Ops.. There is no option like " + startingChoice + " yet.");
+        }
+
+        Cli.closing();
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,35 @@
+package hexlet.code;
+import java.util.Random;
+
+public class Even {
+    public static void getEven() {
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        var countCorrectAnswers = 0;
+        while (countCorrectAnswers < 3) {
+            var randNum = Even.getRandomNumber();
+            System.out.print("Question: " + randNum + "\n");
+            System.out.print("Answer: ");
+            var answer = Cli.getLine().toLowerCase();
+            var correctAnswer = (randNum % 2 == 0) ? "yes" : "no";
+
+            if (answer.equals(correctAnswer)) {
+                countCorrectAnswers++;
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Let's try again, " + Cli.name + "!");
+                break;
+            }
+        }
+        if (countCorrectAnswers == 3) {
+            System.out.println("Congratulations, " + Cli.name + "!");
+        }
+    }
+
+
+    public static int getRandomNumber() {
+        Random random = new Random();
+        var randNum = random.nextInt(100);
+        return randNum;
+    }
+}
