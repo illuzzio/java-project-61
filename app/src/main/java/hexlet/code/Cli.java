02578dd9b25d48a78b550dc702aa7952be280536
Lipatov.Ilya <lipatoff.ilya99@mail.ru>
2024-11-04 00:49:41 +0300

    Modified Cli.java (add getGreet)

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,10 +2,29 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String getName() {
+    public static String name;
+
+    public static String getLine() {
         Scanner scanner = new Scanner(System.in);
-        String userName = scanner.nextLine();
+        String output = scanner.nextLine().trim();
+        return output;
+    }
+
+    public static int getNum() {
+        Scanner scanner = new Scanner(System.in);
+        int output = scanner.nextInt();
+        return output;
+    }
+
+    public static void closing() {
+        Scanner scanner = new Scanner(System.in);
         scanner.close();
-        return userName;
+    }
+
+    public static void getGreet() {
+        System.out.print("May I have your name? ");
+        name = getLine();
+        System.out.println("Hello, " + name + "!");
+
     }
 }
