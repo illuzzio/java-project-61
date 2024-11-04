a3cf833db292d778ccb7c2f981b28812027d4492
Lipatov.Ilya <lipatoff.ilya99@mail.ru>
2024-11-04 19:02:33 +0300

    update Makefile

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Welcome to the 'Brain Games'!");
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
         System.out.print("Your choice: ");
